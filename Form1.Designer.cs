fd135b4a683b834c6fafde0a4fe76a1eeb38ae8e
= <haliler921@gmail.com>
2023-02-28 19:03:40 +0300

    first commit
    commit to main

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace _1190505045_Furkan_Halil_Er_BG
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Izometrik_deger_hesapla_bezier_lstbx = new System.Windows.Forms.ListBox();
+            this.benzier_yuzey_deger_listbx = new System.Windows.Forms.ListBox();
+            this.benzier_yuzey_dusum_bttn = new System.Windows.Forms.Button();
+            this.izometrik_izdusum_txtbx = new System.Windows.Forms.ListBox();
+            this.izometrik_iz_dusum_buton = new System.Windows.Forms.Button();
+            this.kontrol_noktalari_lstbx = new System.Windows.Forms.ListBox();
+            this.kontrol_noktalari_bttn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Izometrik_deger_hesapla_bezier_lstbx
+            // 
+            this.Izometrik_deger_hesapla_bezier_lstbx.FormattingEnabled = true;
+            this.Izometrik_deger_hesapla_bezier_lstbx.ItemHeight = 15;
+            this.Izometrik_deger_hesapla_bezier_lstbx.Location = new System.Drawing.Point(1128, 28);
+            this.Izometrik_deger_hesapla_bezier_lstbx.Name = "Izometrik_deger_hesapla_bezier_lstbx";
+            this.Izometrik_deger_hesapla_bezier_lstbx.Size = new System.Drawing.Size(382, 364);
+            this.Izometrik_deger_hesapla_bezier_lstbx.TabIndex = 13;
+            // 
+            // benzier_yuzey_deger_listbx
+            // 
+            this.benzier_yuzey_deger_listbx.FormattingEnabled = true;
+            this.benzier_yuzey_deger_listbx.ItemHeight = 15;
+            this.benzier_yuzey_deger_listbx.Location = new System.Drawing.Point(731, 28);
+            this.benzier_yuzey_deger_listbx.Name = "benzier_yuzey_deger_listbx";
+            this.benzier_yuzey_deger_listbx.Size = new System.Drawing.Size(382, 364);
+            this.benzier_yuzey_deger_listbx.TabIndex = 12;
+            // 
+            // benzier_yuzey_dusum_bttn
+            // 
+            this.benzier_yuzey_dusum_bttn.Location = new System.Drawing.Point(731, 405);
+            this.benzier_yuzey_dusum_bttn.Name = "benzier_yuzey_dusum_bttn";
+            this.benzier_yuzey_dusum_bttn.Size = new System.Drawing.Size(779, 55);
+            this.benzier_yuzey_dusum_bttn.TabIndex = 11;
+            this.benzier_yuzey_dusum_bttn.Text = "Benzier Yuzey Degerlerini Hesapla // Izometrik Deger Hesapla";
+            this.benzier_yuzey_dusum_bttn.UseVisualStyleBackColor = true;
+            this.benzier_yuzey_dusum_bttn.Click += new System.EventHandler(this.benzier_yuzey_dusum_bttn_Click);
+            // 
+            // izometrik_izdusum_txtbx
+            // 
+            this.izometrik_izdusum_txtbx.FormattingEnabled = true;
+            this.izometrik_izdusum_txtbx.ItemHeight = 15;
+            this.izometrik_izdusum_txtbx.Location = new System.Drawing.Point(387, 29);
+            this.izometrik_izdusum_txtbx.Name = "izometrik_izdusum_txtbx";
+            this.izometrik_izdusum_txtbx.Size = new System.Drawing.Size(274, 364);
+            this.izometrik_izdusum_txtbx.TabIndex = 10;
+            // 
+            // izometrik_iz_dusum_buton
+            // 
+            this.izometrik_iz_dusum_buton.Location = new System.Drawing.Point(387, 405);
+            this.izometrik_iz_dusum_buton.Name = "izometrik_iz_dusum_buton";
+            this.izometrik_iz_dusum_buton.Size = new System.Drawing.Size(274, 55);
+            this.izometrik_iz_dusum_buton.TabIndex = 9;
+            this.izometrik_iz_dusum_buton.Text = "Izometrik Iz Dusum";
+            this.izometrik_iz_dusum_buton.UseVisualStyleBackColor = true;
+            this.izometrik_iz_dusum_buton.Click += new System.EventHandler(this.izometrik_iz_dusum_buton_Click);
+            // 
+            // kontrol_noktalari_lstbx
+            // 
+            this.kontrol_noktalari_lstbx.FormattingEnabled = true;
+            this.kontrol_noktalari_lstbx.ItemHeight = 15;
+            this.kontrol_noktalari_lstbx.Location = new System.Drawing.Point(36, 29);
+            this.kontrol_noktalari_lstbx.Name = "kontrol_noktalari_lstbx";
+            this.kontrol_noktalari_lstbx.Size = new System.Drawing.Size(274, 364);
+            this.kontrol_noktalari_lstbx.TabIndex = 8;
+            // 
+            // kontrol_noktalari_bttn
+            // 
+            this.kontrol_noktalari_bttn.Location = new System.Drawing.Point(36, 405);
+            this.kontrol_noktalari_bttn.Name = "kontrol_noktalari_bttn";
+            this.kontrol_noktalari_bttn.Size = new System.Drawing.Size(274, 55);
+            this.kontrol_noktalari_bttn.TabIndex = 7;
+            this.kontrol_noktalari_bttn.Text = "Kontrol Noktalari";
+            this.kontrol_noktalari_bttn.UseVisualStyleBackColor = true;
+            this.kontrol_noktalari_bttn.Click += new System.EventHandler(this.kontrol_noktalari_bttn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1624, 516);
+            this.Controls.Add(this.Izometrik_deger_hesapla_bezier_lstbx);
+            this.Controls.Add(this.benzier_yuzey_deger_listbx);
+            this.Controls.Add(this.benzier_yuzey_dusum_bttn);
+            this.Controls.Add(this.izometrik_izdusum_txtbx);
+            this.Controls.Add(this.izometrik_iz_dusum_buton);
+            this.Controls.Add(this.kontrol_noktalari_lstbx);
+            this.Controls.Add(this.kontrol_noktalari_bttn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox Izometrik_deger_hesapla_bezier_lstbx;
+        private System.Windows.Forms.ListBox benzier_yuzey_deger_listbx;
+        private System.Windows.Forms.Button benzier_yuzey_dusum_bttn;
+        private System.Windows.Forms.ListBox izometrik_izdusum_txtbx;
+        private System.Windows.Forms.Button izometrik_iz_dusum_buton;
+        private System.Windows.Forms.ListBox kontrol_noktalari_lstbx;
+        private System.Windows.Forms.Button kontrol_noktalari_bttn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1190505045_Furkan_Halil_Er_BG
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        double[,] XCisimNoktaBezierYuzeyKontrolN = new double[20, 4] {  { 0.3, 0.1, 1.5, 1 },
+                                                                        { 0.2, 1.0, 1.0, 1 },
+                                                                        { 0.2, 0.7, 0.8, 1 },
+                                                                        { 0.3, 1.0, 0.2, 1 },
+
+                                                                        { 0.5, 0.8, 1.4, 1 },
+                                                                        { 0.6, 0.6, 1.1, 1 },
+                                                                        { 0.5, 1.2, 0.8, 1 },
+                                                                        { 0.5, 1.0, 0.1, 1 },
+
+                                                                        { 1.2, 1.8, 1.6, 1 },
+                                                                        { 1.1, 0.6, 1.0, 1 },
+                                                                        { 1.2, 1.2, 0.7, 1 },
+                                                                        { 1.3, 1.0, 0.3, 1 },
+
+                                                                        { 1.5, 1.8, 1.5, 1 },
+                                                                        { 1.4, 0.6, 1.0, 1 },
+                                                                        { 1.1, 1.2, 0.7, 1 },
+                                                                        { 0.3, 1.0, 0.1, 1 },
+
+                                                                        { 1.8, 0.4, 1.5, 1 },
+                                                                        { 1.8, 1.6, 1.1, 1 },
+                                                                        { 1.8, 0.7, 0.8, 1 },
+                                                                        { 0.3, 1.0, 0.2, 1 }};
+
+        double[,] Tizometrik = new double[4, 4]
+        {
+            {  0.707 ,   -0.408          ,   0 , 0},
+            {  0     ,    0.816          ,   0 , 0},
+            { -0.707 ,   -0.408          ,   0 , 0},
+            {  0     ,    0              ,   0 , 0}
+        };
+
+        double[,] BenzierNoktalariBulutIzometrik = new double[25, 4];
+        double[,] BenzierYuzeyNoktaBulutu = new double[25, 4];
+        double[,] XCisimNoktaBenzierYuzeyKontrolIzometrik = new double[20, 4];
+        private void kontrol_noktalari_bttn_Click(object sender, EventArgs e)
+        {
+            kontrol_noktalari_lstbx.Items.Add("#\t" + "x\t" + "y\t" + "z\t");
+            for (int i = 0; i < 20; i++)
+            {
+                if (i % 4 == 0) kontrol_noktalari_lstbx.Items.Add("");
+                kontrol_noktalari_lstbx.Items.Add(i + "\t" + XCisimNoktaBezierYuzeyKontrolN[i, 0] + "\t" + XCisimNoktaBezierYuzeyKontrolN[i, 1] + "\t" + XCisimNoktaBezierYuzeyKontrolN[i, 2]);
+            }
+        }
+
+        private void izometrik_iz_dusum_buton_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    for (int k = 0; k < 4; k++)
+                    {
+                        XCisimNoktaBenzierYuzeyKontrolIzometrik[i, j] += XCisimNoktaBezierYuzeyKontrolN[i, k] * Tizometrik[k, j];
+                    }
+                    XCisimNoktaBenzierYuzeyKontrolIzometrik[i, j] = Math.Round(XCisimNoktaBenzierYuzeyKontrolIzometrik[i, j], 4);
+                }
+            }
+
+            izometrik_izdusum_txtbx.Items.Add("#\t" + "x\t" + "y\t" + "z\t");
+            for (int i = 0; i < 20; i++)
+            {
+                if (i % 4 == 0) izometrik_izdusum_txtbx.Items.Add("");
+                izometrik_izdusum_txtbx.Items.Add(i + "\t" + XCisimNoktaBenzierYuzeyKontrolIzometrik[i, 0] + "\t" + XCisimNoktaBenzierYuzeyKontrolIzometrik[i, 1] + "\t" + XCisimNoktaBenzierYuzeyKontrolIzometrik[i, 2]);
+            }
+        }
+
+        private void benzier_yuzey_dusum_bttn_Click(object sender, EventArgs e)
+        {
+            double hassasiyet = 0.25, u = 0, w;
+            benzier_yuzey_deger_listbx.Items.Add("#\t" + "u\t" + "w\t" + "x\t" + "y\t" + "z\t");
+
+            for (int i = 0; i < 5; i++)
+            {
+                w = 0;
+                for (int j = 0; j < 5; j++)
+                {
+                    BenzierYuzeyNoktaBulutu[i * 5 + j, 0] = Math.Round(BezierHesapla(u, w, 0), 4);
+                    BenzierYuzeyNoktaBulutu[i * 5 + j, 1] = Math.Round(BezierHesapla(u, w, 1), 4);
+                    BenzierYuzeyNoktaBulutu[i * 5 + j, 2] = Math.Round(BezierHesapla(u, w, 2), 4);
+                    if ((i * 6 + j) % 6 == 0) benzier_yuzey_deger_listbx.Items.Add("");
+
+                    benzier_yuzey_deger_listbx.Items.Add((i * 5 + j) + "\t" + u + "\t" + w + "\t" + BenzierYuzeyNoktaBulutu[i * 5 + j, 0] + "\t" + BenzierYuzeyNoktaBulutu[i * 5 + j, 1] + "\t" + BenzierYuzeyNoktaBulutu[i * 5 + j, 2]);
+                    w += hassasiyet;
+                }
+                u += hassasiyet;
+            }
+            //2
+            for (int i = 0; i < 25; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    for (int k = 0; k < 4; k++)
+                    {
+                        BenzierNoktalariBulutIzometrik[i, j] += BenzierYuzeyNoktaBulutu[i, k] * Tizometrik[k, j];
+                    }
+                    BenzierNoktalariBulutIzometrik[i, j] = Math.Round(BenzierNoktalariBulutIzometrik[i, j], 4);
+                }
+            }
+
+            Izometrik_deger_hesapla_bezier_lstbx.Items.Add("#\t" + "x\t" + "y\t" + "z\t");
+            //3
+
+            for (int i = 0; i < 25; i++)
+            {
+                if (i % 5 == 0) Izometrik_deger_hesapla_bezier_lstbx.Items.Add("");
+                Izometrik_deger_hesapla_bezier_lstbx.Items.Add(i + "\t" + BenzierNoktalariBulutIzometrik[i, 0] + "\t" + BenzierNoktalariBulutIzometrik[i, 1] + "\t" + BenzierNoktalariBulutIzometrik[i, 2]);
+            }
+        }
+
+        private double BezierHesapla(double uu, double ww, int xyz)
+        {
+            double sonuc = 0, T = 0, K = 0;
+            int n = 4, m = 3;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    T = (Faktor(n) / (Faktor(i) * Faktor(n - i))) * Math.Pow(uu, i) * Math.Pow((1 - uu), (n - i));
+                    K = (Faktor(m) / (Faktor(j) * Faktor(m - j))) * Math.Pow(ww, j) * Math.Pow((1 - ww), (m - j));
+                    sonuc += XCisimNoktaBezierYuzeyKontrolN[i * 3 + j, xyz] * T * K;
+                }
+            }
+            return sonuc;
+        }
+        //5
+
+        #region Fonk Metod
+        private double Faktor(double sayi)
+        {
+            double sonuc = 1;
+            for (int i = 1; i <= sayi; i++)
+            { sonuc *= (double)i; }
+            return sonuc;
+        }
+        #endregion
+
+    }
+}
